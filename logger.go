a9fb621573abbfd59733a208c90ed95fa6887ded
Sergey <serge@cheebo.me>
2019-12-12 00:28:45 +0500

    fixed interface and logger methods

--- logger.go
+++ logger.go
@@ -31,8 +31,8 @@ type LevelEnabler interface {
 	Enabled(logger logger.Level) bool
 }
 
-func New() (logger logger.FieldLogger) {
-	return Logger{
+func New() (logger logger.Logger) {
+	return &Logger{
 		Out:          os.Stderr,
 		mu:           sync.Mutex{},
 		Core:         Core{},
@@ -44,59 +44,52 @@ func New() (logger logger.FieldLogger) {
 }
 
 // Fatal
-func (log Logger) Panic(format string, opts ...interface{}) {
+func (log *Logger) Panic(format string, opts ...interface{}) {
 	log.Log(logger.LevelPanic, format, opts...)
 
 }
 
 // Fatal logs a message with fatal level and exit with status set to 1
-func (log Logger) Fatal(format string, opts ...interface{}) {
+func (log *Logger) Fatal(format string, opts ...interface{}) {
 	log.Log(logger.LevelFatal, format, opts...)
 
 }
 
 // Critical push to log entry with critical level
-func (log Logger) Critical(format string, opts ...interface{}) {
+func (log *Logger) Critical(format string, opts ...interface{}) {
 	log.Log(logger.LevelCritical, format, opts...)
 }
 
 // Error push to log entry with error level
-func (log Logger) Error(format string, opts ...interface{}) {
+func (log *Logger) Error(format string, opts ...interface{}) {
 	log.Log(logger.LevelError, format, opts...)
 
 }
 
 // Warning push to log entry with warning level
-func (log Logger) Warning(format string, opts ...interface{}) {
+func (log *Logger) Warning(format string, opts ...interface{}) {
 	log.Log(logger.LevelWarning, format, opts...)
 
 }
 
 // Notice push to log entry with notice level
-func (log Logger) Notice(format string, opts ...interface{}) {
+func (log *Logger) Notice(format string, opts ...interface{}) {
 	log.Log(logger.LevelNotice, format, opts...)
 
 }
 
 // Info push to log entry with info level
-func (log Logger) Info(format string, opts ...interface{}) {
+func (log *Logger) Info(format string, opts ...interface{}) {
 	log.Log(logger.LevelInfo, format, opts...)
 }
 
 // Debug push to log entry with debug level
-func (log Logger) Debug(format string, opts ...interface{}) {
+func (log *Logger) Debug(format string, opts ...interface{}) {
 	log.Log(logger.LevelDebug, format, opts...)
 }
 
-// Printf is like fmt.Printf, push to log entry with debug level
-func (log Logger) Printf(format string, opts ...interface{}) {
-	log.mu.Lock()
-	defer log.mu.Unlock()
-	log.Out.Write([]byte(fmt.Sprintf(format, opts...)))
-}
-
 // Log push to log with specified level
-func (log Logger) Log(level logger.Level, format string, opts ...interface{}) {
+func (log *Logger) Log(level logger.Level, format string, opts ...interface{}) {
 	log.mu.Lock()
 	defer log.mu.Unlock()
 
@@ -115,7 +108,7 @@ func (log Logger) Log(level logger.Level, format string, opts ...interface{}) {
 
 }
 
-func (log *Logger) With(fields ...logger.Field) *Logger {
+func (log *Logger) With(fields ...logger.Field) logger.Logger {
 	if len(fields) == 0 {
 		return log
 	}
